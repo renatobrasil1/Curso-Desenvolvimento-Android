74e74882d15ecf2eb7ee5d18e1976593902b1aa7
renatobrasil1 <nato-brasil@hotmail.com>
2023-06-18 17:08:35 -0300

    executando o aplicativo no device ou seja no celular

---
+++ Applistamotociclista/app/src/main/java/renato/brasil/com/br/applistamotociclista/MainActivity.java
@@ -0,0 +1,12 @@
+package renato.brasil.com.br.applistamotociclista;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
