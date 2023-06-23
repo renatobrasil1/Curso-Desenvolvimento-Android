7b9b71706d4b9fbab6cfbb0b0331844cb7a9c6fd
renatobrasil1 <nato-brasil@hotmail.com>
2023-06-23 16:20:31 -0300

    limpando dados no sharedPreferences ao clicar no botão limpar

--- Applistamotociclista/app/src/main/java/renato/brasil/com/br/applistamotociclista/MainActivity.java
+++ Applistamotociclista/app/src/main/java/renato/brasil/com/br/applistamotociclista/MainActivity.java
@@ -15,6 +15,8 @@ import renato.brasil.com.br.applistamotociclista.model.Pessoa;
 public class MainActivity extends Activity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip; // aqui eu estou deixando com publico
+
     public static final String NOME_PREFERENCES = "pref_listamotociclista";
 
     PessoaController controller; // estou criando uma estância , ate o momento ela e null
@@ -27,7 +29,7 @@ public class MainActivity extends Activity {
     EditText editQualProfissional;
     EditText editTelefoneDeContato;
 
-    Button btnLimpar;
+    Button btnLimpar; //Ctrl + B a gente busca a classe
     Button btnSalvar;
     Button btnFinalizar;
 
@@ -36,19 +38,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preferences = getSharedPreferences(NOME_PREFERENCES,0); // o zero é para leitura e escrita
-         SharedPreferences.Editor listaVip = preferences.edit(); // criando o editor
+        preferences = getSharedPreferences(NOME_PREFERENCES, 0); // o zero é para leitura e escrita
+        listaVip = preferences.edit(); // criando o editor
 
 
-       controller = new PessoaController(); // criando o objeto controller
+        controller = new PessoaController(); // criando o objeto controller
         controller.toString();
 
         pessoa = new Pessoa(); // criando um objeto pessoa
 
-          pessoa.setPrimeiroNome(preferences.getString("primmeiroNome","não existe ..."));
-          pessoa.setSobreNome(preferences.getString("sobreNome","NA"));
-          pessoa.setProfissionalDesejado(preferences.getString("qualProfissional","NA"));
-          pessoa.setTelefoneContato(preferences.getString("telefoneContato","NA"));
+        pessoa.setPrimeiroNome(preferences.getString("primmeiroNome", "não existe ..."));
+        pessoa.setSobreNome(preferences.getString("sobreNome", "NA"));
+        pessoa.setProfissionalDesejado(preferences.getString("qualProfissional", "NA"));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", "NA"));
 
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
@@ -73,6 +75,9 @@ public class MainActivity extends Activity {
                 editSobreNome.setText("");
                 editQualProfissional.setText("");
                 editTelefoneDeContato.setText("");
+
+                listaVip.clear(); // estou limpando o campo
+                listaVip.apply(); // salvando os dados
             }
         });
 
@@ -85,8 +90,7 @@ public class MainActivity extends Activity {
         });
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
                 pessoa.setSobreNome(editSobreNome.getText().toString());
                 pessoa.setProfissionalDesejado(editQualProfissional.getText().toString());
@@ -95,10 +99,10 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "Salvo" + pessoa.toString(), Toast.LENGTH_LONG).show();
                 finish();
 
-                listaVip.putString("primmeiroNome",pessoa.getPrimeiroNome());
-                listaVip.putString("sobreNome",pessoa.getSobreNome());
-                listaVip.putString("qualProfissional",pessoa.getProfissionalDesejado());
-                listaVip.putString("telefoneContato",pessoa.getTelefoneContato());
+                listaVip.putString("primmeiroNome", pessoa.getPrimeiroNome());
+                listaVip.putString("sobreNome", pessoa.getSobreNome());
+                listaVip.putString("qualProfissional", pessoa.getProfissionalDesejado());
+                listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
 
                 listaVip.apply(); // estou salvando o contato
 
