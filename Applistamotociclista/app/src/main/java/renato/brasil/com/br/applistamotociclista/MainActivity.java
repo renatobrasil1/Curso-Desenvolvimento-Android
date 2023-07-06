a0a7134030bfa1cefc27efa4214a1b366d249409
renatobrasil1 <nato-brasil@hotmail.com>
2023-07-06 14:58:52 -0300

    Adicinando algumas anotaçoes

--- Applistamotociclista/app/src/main/java/renato/brasil/com/br/applistamotociclista/MainActivity.java
+++ Applistamotociclista/app/src/main/java/renato/brasil/com/br/applistamotociclista/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spinner);
+        setContentView(R.layout.activity_spinner); // ao iniciar chama atela activity_spinner
 
         preferences = getSharedPreferences(NOME_PREFERENCES,0); // o zero é para leitura e escrita
          SharedPreferences.Editor listaVip = preferences.edit(); // criando o editor
