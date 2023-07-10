78e09bdba63ab6bf9ff8ba1e8a3bf6dbf24be6b1
renatobrasil1 <nato-brasil@hotmail.com>
2023-07-10 12:04:17 -0300

    Criado o aplicativo AppGasEta significado Gasolina Etanol

---
+++ 01Applistamotociclista/app/build/generated/source/buildConfig/debug/renato/brasil/com/br/applistamotociclista/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package renato.brasil.com.br.applistamotociclista;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "renato.brasil.com.br.applistamotociclista";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ 02AppGasEta/app/build/generated/source/buildConfig/release/renato/brasil/com/br/appgaseta/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package renato.brasil.com.br.appgaseta;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "renato.brasil.com.br.appgaseta";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
